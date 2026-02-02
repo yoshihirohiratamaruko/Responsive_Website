74e6c862465b3c5d0457c41fb87cf91e3a2bbbbd
hirata <quruli9018@gmail.com>
2026-02-02 10:54:34 +0900

    レスポンシブWebサイト完成

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ globalNavBtn.addEventListener('click', () => {
 globalNavCloseBtn.addEventListener('click', () => {
     document.body.classList.remove('is-open');
 });
+
+// FAQアコーディオン
+const faqItems = document.querySelectorAll('.p-faq__listItem');
+faqItems.forEach((item) => {
+    const btn = item.querySelector('.p-faq__listBtn');
+    const answer = item.querySelector('.p-faq__listA');
+
+    btn.addEventListener('click', () => {
+        answer.classList.toggle('is-open');
+    })
+});
